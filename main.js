43025098f5cdcd20ac9d5decb55254b8873080db
andreamazza84 <andrea.mazza84@gmail.com>
2020-12-09 17:42:16 +0100

    JSnack 2 - Blocco 4

--- main.js
+++ main.js
@@ -3,20 +3,49 @@
 //Nome = palla
 //Peso = 10
 
-var oggetto = {
-    'nome': 'palla',
-    'peso': 10
-};
+// var oggetto = {
+//     'nome': 'palla',
+//     'peso': 10
+// };
 
 //JSnack 1B
 //Attraverso un prompt dare la possibilità all’utente di
 //modificare il peso della palla.
 
-oggetto.peso = Number(prompt("Inserisci il nuovo peso per l'oggetto palla"));
-console.log(oggetto);
+// oggetto.peso = Number(prompt("Inserisci il nuovo peso per l'oggetto palla"));
+// console.log(oggetto);
 
+//JSnack 2
+//Creare un array di oggetti: ogni oggetto descriverà una bici
+//da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
 
+biciclette = [
+    {'nome': 'bianchi',
+    'peso': '4800'},
+    {'nome': 'conalgo',
+    'peso': '3700'},
+    {'nome': 'adriatica',
+    'peso': '5000'},
+    {'nome': 'marca-x',
+    'peso': '4000'},
+    {'nome': 'marca-y',
+    'peso': '3200'},
+    {'nome': 'marca-z',
+    'peso': '3600'},
+];
 
+let min = biciclette[0]['peso'];
+let marcaMin = "";
+for (let index = 1; index < biciclette.length; index++) {
+    
+    if(min > biciclette[index]['peso']){
+        min = biciclette[index]['peso'];
+        marcaMin = biciclette[index]['nome'];
+    }    
+}
+
+console.log(`La bici di marca ${marcaMin} è la più leggera e pesa ${min} g`);
 
 //Scrivi una funzione che accetti una stringa 
 //come argomento e la ritorni girata 
